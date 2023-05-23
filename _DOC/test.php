90d25e54bf488f61d995280db0de9f2a8907fa3a
JuuLed <julien.ledoux@epsi.fr>
2023-05-23 11:38:42 +0200

    maj bdd

---
+++ _DOC/test.php
@@ -0,0 +1,133 @@
+<!-- Mecanique
+Medical
+Musique et son
+Nature
+Noel
+Nourriture et Boisson
+Objet
+Photo - video
+Securite
+Signe et Symbol
+Sport
+Symboles
+Temps - meteo
+Transport
+Vacances
+Animal
+Anniversaire
+Arme
+Batiment et pays
+Boite
+Bulle de parole
+Carte et localisation
+Communication et Reseau
+Doigt
+Ecole
+Enfant
+Entreprise et Finance
+Espace
+Evenement
+Fichier
+Fleche
+Format de fichier
+Habille et accessoire
+Humain
+Illustrations pour gobelets
+Industrie
+informatique
+Insectes
+Maison
+Mariage -->
+
+
+_____________________________________________________________________________________________________________________
+// LanguageController.php
+
+
+_____________________________________________________________________________________________________________________
+// CategoryController.php
+
+
+_____________________________________________________________________________________________________________________
+// KeywordController.php
+
+
+_____________________________________________________________________________________________________________________
+// SymbolController.php
+
+
+_____________________________________________________________________________________________________________________
+// Language.php
+
+
+_____________________________________________________________________________________________________________________
+// Category.php
+
+
+_____________________________________________________________________________________________________________________
+// Keyword.php
+
+
+
+_____________________________________________________________________________________________________________________
+// Symbol.php
+<?php
+class Symbol {
+    protected $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function getAllSymbols() {
+        $query = "SELECT * FROM symbols";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getSymbolById($id) {
+        $query = "SELECT * FROM symbols WHERE symbol_id = :id";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function createSymbol($fileName, $size, $active, $deleted) {
+        $query = "INSERT INTO symbols (file_name, size, active, deleted) VALUES (:fileName, :size, :active, :deleted)";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':fileName', $fileName);
+        $stmt->bindParam(':size', $size);
+        $stmt->bindParam(':active', $active, PDO::PARAM_BOOL);
+        $stmt->bindParam(':deleted', $deleted, PDO::PARAM_BOOL);
+        $stmt->execute();
+
+        return $this->pdo->lastInsertId();
+    }
+
+    public function updateSymbol($id, $fileName, $size, $active, $deleted) {
+        $query = "UPDATE symbols SET file_name = :fileName, size = :size, active = :active, deleted = :deleted WHERE symbol_id = :id";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':fileName', $fileName);
+        $stmt->bindParam(':size', $size);
+        $stmt->bindParam(':active', $active, PDO::PARAM_BOOL);
+        $stmt->bindParam(':deleted', $deleted, PDO::PARAM_BOOL);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+
+        return $stmt->rowCount();
+    }
+
+    public function deleteSymbol($id) {
+        $query = "DELETE FROM symbols WHERE symbol_id = :id";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+
+        return $stmt->rowCount();
+    }
+}
+?>
