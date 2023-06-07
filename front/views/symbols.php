c415ae7e399e5ffa31095b935808d54b03bf29d1
JuuLed <julien.ledoux@epsi.fr>
2023-06-07 12:54:02 +0200

    on/off

--- front/views/symbols.php
+++ front/views/symbols.php
@@ -67,7 +67,6 @@
 		width: 10%;
 	}
 
-	.save-btn,
 	.delete-btn {
 		width: 15vh;
 		margin: 0.2vh;
@@ -81,7 +80,6 @@
 		border-radius: 1vh;
 	}
 
-	.save-btn:hover,
 	.delete-btn:hover {
 		border: 2px inset grey;
 	}
@@ -106,6 +104,44 @@
 		outline: none;
 		border: 1px solid grey;
 		border-radius: 0.2vh;
+	}
+
+
+	/* Style pour le bouton ON/OFF avec curseur glissant type Apple */
+	.toggle-btn {
+		position: relative;
+		display: inline-block;
+		width: 4em;
+		height: 2em;
+		background-color: #ddd;
+		border-radius: 1em;
+		cursor: pointer;
+		transition: background-color 0.3s ease;
+		overflow: hidden;
+	}
+
+	.toggle-btn::before {
+		content: '';
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		left: 0.3em;
+		width: 1.4em;
+		height: 1.4em;
+		background-color: rgba(255, 255, 255, 0.5);
+		border-radius: 50%;
+		border: 1px solid grey;
+		box-shadow: 0 0.1em 0.2em rgba(0, 0, 0, 0.2);
+		transition: transform 0.3s ease;
+	}
+
+	.toggle-btn.active {
+		background-color: #4cd964;
+		color: white;
+	}
+
+	.toggle-btn.active::before {
+		transform: translateX(1.8em) translateY(-50%);
 	}
 
 	/* Mise en page pour les catégories */
@@ -257,7 +293,7 @@
 		<label for="categoryFilter">Filtrer par catégorie :</label>
 		<select id="categoryFilter">
 			<option value="">Toutes les catégories</option>
-			<!-- Les options de catégorie seront ajoutées ici par JavaScript -->
+			<!-- Les options de catégorie seront ajoutées ici en JavaScript -->
 		</select>
 	</div>
 
@@ -272,7 +308,7 @@
 		<th>Mots-clés</th>
 		<th>Actions</th>
 	</tr>
-	<!-- Les lignes du tableau seront ajoutées ici par JavaScript -->
+	<!-- Les lignes du tableau seront ajoutées ici en JavaScript -->
 </table>
 
 
@@ -332,10 +368,10 @@
 			</div>
 		  </td>
 
-		  <td class="cln-size contenteditable" data-field="active">
-			<div class="editable-content" contenteditable="true">
-			  ${symbol.active}
-			</div>
+		  <td class="cln-size" data-field="active">
+			<button class="toggle-btn ${symbol.active ? 'active' : ''}" data-id="${symbol.id}">
+			  ${symbol.active ? 'ON' : 'OFF'}
+			</button>
 		  </td>
 
 		  <td>
@@ -369,7 +405,6 @@
 		  </td>
 
 		  <td>
-			<button class="save-btn" data-id="${symbol.id}">Enregistrer</button>
 			<button class="delete-btn" data-id="${symbol.id}">Supprimer</button>
 		  </td>
 		</tr>
@@ -378,6 +413,7 @@
 
 		$('.table-symbols').append(tableContent);
 	});
+
 
 	// _____________________________MODAL__________________________________
 
@@ -472,7 +508,7 @@
 
 	function addItem(itemElement, itemType) {
 		var symbolRow = itemElement.closest("tr");
-		var symbolId = symbolRow.querySelector(".save-btn").dataset.id;
+		var symbolId = symbolRow.querySelector(".delete-btn").dataset.id;
 		var itemId = itemElement.dataset[itemType + "Id"];
 		var apiUrl = apiBaseURL + "/symbols/" + symbolId;
 
@@ -522,12 +558,12 @@
 		});
 	}
 
-	// ___________________________________________________________________________
+	// __________________________________ Supprimer la liaison categories ou mots-clés sur les symboles _________________________________________
 	function deleteCategory() {
 		var categoryItem = this.closest("li");
 		var categoryId = categoryItem.dataset.categoryId;
 		var symbolRow = this.closest("tr");
-		var symbolId = symbolRow.querySelector(".save-btn").dataset.id;
+		var symbolId = symbolRow.querySelector(".delete-btn").dataset.id;
 
 		// Effectuer une requête AJAX DELETE pour supprimer la catégorie du symbole
 		$.ajax({
@@ -547,7 +583,7 @@
 		var keywordItem = this.closest("li");
 		var keywordId = keywordItem.dataset.keywordId;
 		var symbolRow = this.closest("tr");
-		var symbolId = symbolRow.querySelector(".save-btn").dataset.id;
+		var symbolId = symbolRow.querySelector(".delete-btn").dataset.id;
 
 		// Effectuer une requête AJAX DELETE pour supprimer le mot-clé du symbole
 		$.ajax({
@@ -563,26 +599,57 @@
 		});
 	}
 
-	// Attacher des gestionnaires d'événements aux éléments du tableau
-	$(document).on("click", ".add-category-btn", openModal);
-	$(document).on("click", ".add-keyword-btn", openModal);
-	$(document).on("click", ".delete-category-btn", deleteCategory);
-	$(document).on("click", ".delete-keyword-btn", deleteKeyword);
+	//________________ Fonction pour gérer le basculement du bouton ON/OFF ______________
+	function toggleActive(symbolId) {
+		var toggleButton = $(`.toggle-btn[data-id="${symbolId}"]`);
+		var isActive = toggleButton.hasClass("active") ? 0 : 1; // Convertir la valeur en entier
 
-	// Attacher des gestionnaires d'événements pour enregistrer modifications et supprimer les symboles
-	$(document).on("click", ".save-btn", function () {
+		// Effectuer une requête AJAX PUT pour mettre à jour l'état actif du symbole
+		$.ajax({
+			url: apiBaseURL + "/symbols/" + symbolId,
+			type: "PUT",
+			data: JSON.stringify({
+				active: isActive === 1 ? 1 : 0 // Assurer que la valeur est soit 1 soit 0
+			}),
+			contentType: "application/json",
+			success: function (data) {
+				console.log("État actif du symbole mis à jour avec succès:", data);
+				// Mettre à jour le bouton ON/OFF et son état actif
+				toggleButton.toggleClass("active");
+				toggleButton.text(toggleButton.hasClass("active") ? "ON" : "OFF");
+
+				// Mettre à jour le symbole dans le tableau
+				var symbolRow = toggleButton.closest("tr");
+				updateSymbol(symbolId, symbolRow);
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+				console.log("Erreur lors de la mise à jour de l'état actif du symbole:", textStatus, errorThrown);
+			}
+		});
+	}
+
+	// Gestionnaire d'événement pour le basculement de l'état actif
+	$(document).on("click", ".toggle-btn", function () {
 		var symbolId = $(this).data("id");
-		var symbolRow = $(this).closest("tr");
-		var fileName = symbolRow.find("[data-field='file_name'] .editable-content").text().trim();
-		var size = symbolRow.find("[data-field='size'] .editable-content").text().trim();
-		var active = symbolRow.find("[data-field='active'] .editable-content").text().trim();
+		var isActive = $(this).hasClass("active");
 
-		console.log(symbolRow);
+		toggleActive(symbolId, !isActive);
+	});
+
+	//_____________________ UPDATE SYMBOLE ____________________________________
+	// Fonction pour gérer la mise à jour du symbole
+	function updateSymbol(symbolId, symbolRow) {
+		var fileNameElement = symbolRow.find("[data-field='file_name'] .editable-content");
+		var sizeElement = symbolRow.find("[data-field='size'] .editable-content");
+		var activeElement = symbolRow.find("[data-field='active'] .toggle-btn");
+
+		var fileName = fileNameElement.text().trim();
+		var size = parseInt(sizeElement.text().trim());
+		var active = activeElement.hasClass("active") ? 1 : 0;
 
 		var categories = Array.from(symbolRow[0].querySelectorAll(".category-list li[data-category-id]")).map(function (category) {
 			return parseInt(category.dataset.categoryId);
 		});
-		console.log(categories);
 
 		var keywords = Array.from(symbolRow[0].querySelectorAll(".keyword-list li")).map(function (keyword) {
 			return parseInt(keyword.dataset.keywordId);
@@ -594,8 +661,8 @@
 			type: "PUT",
 			data: JSON.stringify({
 				file_name: fileName,
-				size: parseInt(size),
-				active: parseInt(active),
+				size: size,
+				active: active,
 				categories: categories,
 				keywords: keywords
 			}),
@@ -607,24 +674,52 @@
 				console.log("Erreur lors de la mise à jour du symbole:", textStatus, errorThrown);
 			}
 		});
+	}
+
+	// Attacher un gestionnaire d'événement à la modification du champ "Nom du fichier"
+	$(document).on("input", "[data-field='file_name'] .editable-content", function () {
+		var symbolId = $(this).closest("tr").find(".delete-btn").data("id");
+		var symbolRow = $(this).closest("tr");
+		updateSymbol(symbolId, symbolRow);
 	});
 
+	// Attacher un gestionnaire d'événement à la modification du champ "Taille"
+	$(document).on("input", "[data-field='size'] .editable-content", function () {
+		var symbolId = $(this).closest("tr").find(".delete-btn").data("id");
+		var symbolRow = $(this).closest("tr");
+		updateSymbol(symbolId, symbolRow);
+	});
+
+
+
+	//___________________ gestionnaires d'événements aux éléments du tableau _______________________
+	$(document).on("click", ".add-category-btn", openModal);
+	$(document).on("click", ".add-keyword-btn", openModal);
+	$(document).on("click", ".delete-category-btn", deleteCategory);
+	$(document).on("click", ".delete-keyword-btn", deleteKeyword);
+
+
+
+	//__________________ SUPPRESSION D'UN SYMBOLE ______________________
 	$(document).on("click", ".delete-btn", function () {
 		var symbolId = $(this).data("id");
 		var symbolRow = $(this).closest("tr");
 
-		// Effectuer une requête AJAX DELETE pour supprimer le symbole
-		$.ajax({
-			url: apiBaseURL + "/symbols/" + symbolId,
-			type: "DELETE",
-			success: function () {
-				console.log("Symbole supprimé avec succès.");
-				symbolRow.remove();
-			},
-			error: function (jqXHR, textStatus, errorThrown) {
-				console.log("Erreur lors de la suppression du symbole:", textStatus, errorThrown);
-			}
-		});
+		// Afficher une boîte de dialogue de confirmation avant la suppression
+		if (confirm("Êtes-vous sûr de vouloir supprimer ce symbole ?")) {
+			// Effectuer une requête AJAX DELETE pour supprimer le symbole
+			$.ajax({
+				url: apiBaseURL + "/symbols/" + symbolId,
+				type: "DELETE",
+				success: function () {
+					console.log("Symbole supprimé avec succès.");
+					symbolRow.remove();
+				},
+				error: function (jqXHR, textStatus, errorThrown) {
+					console.log("Erreur lors de la suppression du symbole:", textStatus, errorThrown);
+				}
+			});
+		}
 	});
 
 
--- front/views/test_ajax.php
+++ front/views/test_ajax.php
@@ -1,145 +1,47 @@
 <script>
 
-function openModal() {
-		var symbolRow = this.closest("tr");
-		var categoryList = symbolRow.querySelector("#categoryList");
 
-		fetch(apiBaseURL + "/categories")
-			.then(function (response) {
-				if (response.ok) {
-					return response.json();
-				}
-				throw new Error("Erreur lors de la récupération des catégories.");
-			})
-			.then(function (categories) {
-				var existingCategories = Array.from(symbolRow.querySelectorAll("[data-category-id]")).map(function (category) {
-					return category.dataset.categoryId;
-				});
+	//* Attacher des gestionnaires d'événements pour enregistrer modifications et supprimer les symboles
+	$(document).on("click", ".save-btn", function () {
+		var symbolId = $(this).data("id");
+		var symbolRow = $(this).closest("tr");
+		var fileName = symbolRow.find("[data-field='file_name'] .editable-content").text().trim();
+		var size = symbolRow.find("[data-field='size'] .editable-content").text().trim();
+		var active = symbolRow.find("[data-field='active'] .editable-content").text().trim();
 
-				categoryList.innerHTML = "";
+		console.log(symbolRow);
 
-				categories.forEach(function (category) {
-					if (!existingCategories.includes(category.category_id.toString())) {
-						var categoryItem = document.createElement("li");
-						categoryItem.dataset.categoryId = category.category_id;
-						categoryItem.textContent = category.category;
+		var categories = Array.from(symbolRow[0].querySelectorAll(".category-list li[data-category-id]")).map(function (category) {
+			return parseInt(category.dataset.categoryId);
+		});
+		console.log(categories);
 
-						categoryItem.addEventListener("click", function () {
-							// Code à exécuter lors du clic sur la catégorie
-							// Par exemple, vous pouvez ajouter une classe ou effectuer une action spécifique
-							console.log("Catégorie cliquée :", category.category);
+		var keywords = Array.from(symbolRow[0].querySelectorAll(".keyword-list li")).map(function (keyword) {
+			return parseInt(keyword.dataset.keywordId);
+		});
 
-							addCategory(categoryItem);
-						});
-
-						categoryItem.addEventListener("mouseover", handleCategoryMouseOver);
-						categoryItem.addEventListener("mouseout", handleCategoryMouseOut);
-
-						categoryList.appendChild(categoryItem);
-					}
-				});
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
-
-		var keywordList = symbolRow.querySelector("#keywordList");
-
-		fetch(apiBaseURL + "/keywords")
-			.then(function (response) {
-				if (response.ok) {
-					return response.json();
-				}
-				throw new Error("Erreur lors de la récupération des mots-clés.");
-			})
-			.then(function (keywords) {
-				var existingKeywords = Array.from(symbolRow.querySelectorAll("[data-keyword-id]")).map(function (keyword) {
-					return keyword.dataset.keywordId;
-				});
-
-				keywordList.innerHTML = "";
-
-				keywords.forEach(function (keyword) {
-					if (!existingKeywords.includes(keyword.keyword_id.toString())) {
-						var keywordItem = document.createElement("li");
-						keywordItem.dataset.keywordId = keyword.keyword_id;
-						keywordItem.textContent = keyword.keyword;
-
-						keywordItem.addEventListener("click", function () {
-							console.log("Mot-clé cliqué :", keyword.keyword);
-							addKeyword(keywordItem);
-						});
-
-						keywordItem.addEventListener("mouseover", handleKeywordMouseOver);
-						keywordItem.addEventListener("mouseout", handleKeywordMouseOut);
-
-						keywordList.appendChild(keywordItem);
-					}
-				});
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
-
-		var modal = symbolRow.querySelector(".modal");
-		modal.style.display = "block";
-
-		// Fermer la modal lorsque l'utilisateur clique en dehors de celle-ci
-		window.onclick = function (event) {
-			if (event.target == modal) {
-				closeModal();
+		// Effectuer une requête AJAX PUT pour enregistrer les modifications du symbole
+		$.ajax({
+			url: apiBaseURL + "/symbols/" + symbolId,
+			type: "PUT",
+			data: JSON.stringify({
+				file_name: fileName,
+				size: parseInt(size),
+				active: parseInt(active),
+				categories: categories,
+				keywords: keywords
+			}),
+			contentType: "application/json",
+			success: function (data) {
+				console.log("Symbole mis à jour avec succès:", data);
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+				console.log("Erreur lors de la mise à jour du symbole:", textStatus, errorThrown);
 			}
-		};
-	}
+		});
+	});
 
 
 
-
-
-//! fonction a tester !!!!!!!!
-	function addItem(itemElement, itemType) {
-  var symbolRow = itemElement.closest("tr");
-  var symbolId = symbolRow.querySelector(".save-btn").dataset.id;
-  var itemId = itemElement.dataset[itemType + "Id"];
-
-  // Effectuer une requête AJAX POST pour associer l'élément au symbole en base de données
-  $.ajax({
-    url: apiBaseURL + "/symbols/" + symbolId + "/" + itemType + "s/" + itemId,
-    type: "POST",
-    success: function (data) {
-      console.log(itemType.charAt(0).toUpperCase() + itemType.slice(1) + " associé au symbole avec succès:", data);
-
-      var itemContainer = symbolRow.querySelector("." + itemType + "-list");
-
-      var newItemElement = document.createElement("li");
-      newItemElement.dataset[itemType + "Id"] = itemId;
-
-      var itemSpan = document.createElement("span");
-      itemSpan.textContent = itemElement.textContent;
-      newItemElement.appendChild(itemSpan);
-
-      var deleteButton = document.createElement("button");
-      deleteButton.classList.add("delete-" + itemType + "-btn");
-      deleteButton.innerHTML = "&times;";
-      newItemElement.appendChild(deleteButton);
-
-      var addButton = itemContainer.querySelector(".add-btn");
-
-      // Insérer le nouvel élément avant le bouton "+"
-      itemContainer.insertBefore(newItemElement, addButton);
-
-      // Effacer la sélection
-      itemElement.classList.remove(itemType + "-hover");
-
-      // Retirer l'élément de la liste
-      itemElement.remove();
-
-      // closeModal();
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      console.log("Erreur lors de l'association de l'" + itemType + " au symbole:", textStatus, errorThrown);
-    }
-  });
-}
 
 </script>
